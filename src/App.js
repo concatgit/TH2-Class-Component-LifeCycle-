0c4438dd5b076c518bec5c854847b145bcf5179f
concatgit <buibang147@gmail.com>
2023-02-14 20:54:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Box from './Box';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={idDisplay:true};
+  }
+  onRemoveBox=()=>
+  {
+    this.setState({idDisplay:false});
+  }
+  render()
+  {
+    return (
+      <div>
+        {this.state.idDisplay && <Box/>}
+        <div onClick={this.onRemoveBox}>Xóa component Box</div>
+      </div>
+    )
+  }
 }
 
 export default App;
